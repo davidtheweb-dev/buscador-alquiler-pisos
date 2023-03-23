a4830a0959912b82cb5a0c557e5581d7e43ac452
davidheweb <davidhermosilladuro@gmail.com>
2023-03-23 11:52:01 +0100

    Registered routes and added route page components
    Paths:
    '/'
    '/viviendas'
    '/viviendas/:id'
    '/viviendas/:id/contacto'
    'registro'
    'solicitudes'
    '/:notFound(.*)'

---
+++ src/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HousingList from './pages/housing/HousingList.vue';
+import HousingDetail from './pages/housing/HousingDetail.vue';
+import HousingContact from './pages/housing/HousingContact.vue';
+import HousingRegistration from './pages/housing/HousingRegistration.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/viviendas' },
+    { path: '/viviendas', component: HousingList },
+    {
+      path: '/viviendas/:id',
+      component: HousingDetail,
+      children: [
+        { path: '/contacto', component: HousingContact }, // /viviendas/id/contacto
+      ],
+    },
+    { path: '/registro', component: HousingRegistration },
+    { path: '/solicitudes', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
