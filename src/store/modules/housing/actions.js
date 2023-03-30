bd13ea2c728d288785103501f5e202b03dce5edc
davidheweb <davidhermosilladuro@gmail.com>
2023-03-30 19:07:51 +0200

    Sending a PUT http request to store housing data

--- src/store/modules/housing/actions.js
+++ src/store/modules/housing/actions.js
@@ -1,13 +1,30 @@
 export default {
-  registerHousing(context, data) {
+  async registerHousing(context, data) {
+    const userId = context.rootGetters.userId;
     const housingData = {
-      id: context.rootGetters.userId,
       title: data.title,
       description: data.description,
       rate: data.rate,
       tags: data.tags,
     };
 
-    context.commit('registerHousing', housingData);
+    const response = await fetch(
+      `https://student-rent-finder-default-rtdb.europe-west1.firebasedatabase.app/housing/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(housingData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // TODO: error
+    }
+
+    context.commit('registerHousing', {
+      ...housingData,
+      id: userId,
+    });
   },
 };
