ac5e14eb8b2107f55bb92d7333650ab40545fd1a
davidheweb <davidhermosilladuro@gmail.com>
2023-03-31 12:19:01 +0200

    Preparing vuex for auth

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,13 @@ import { createStore } from 'vuex';
 
 import housingModule from './modules/housing/index.js';
 import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
   modules: {
     housing: housingModule,
     requests: requestsModule,
-  },
-  state() {
-    return {
-      userId: '3',
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    },
+    auth: authModule,
   },
 });
 
