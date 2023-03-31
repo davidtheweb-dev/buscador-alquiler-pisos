b16549dd3d753a5705a7e48d26947fc44cb647cf
davidheweb <davidhermosilladuro@gmail.com>
2023-03-31 11:14:54 +0200

    Added the "not found" page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  NOT FOUND
+  <section>
+    <base-card>
+      <h2>¡Ups! Página no encontrada</h2>
+      <p>
+        Lo sentimos, pero la página que buscas no existe. Prueba a mirar
+        nuestros <router-link to="/viviendas">pisos</router-link>.
+      </p>
+    </base-card>
+  </section>
 </template>
